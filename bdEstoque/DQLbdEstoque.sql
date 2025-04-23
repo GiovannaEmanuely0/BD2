0ce0bd6c14701f4fc7f989357094d4638b95634f
Giovanna Emanuely <giovannaEmma16@gmail.com>
2025-04-23 19:45:28 -0300

    Update DQLbdEstoque.sql

--- bdEstoque/DQLbdEstoque.sql
+++ bdEstoque/DQLbdEstoque.sql
@@ -1,11 +1,12 @@
 ---dql bd estoque
+---ExercÃ­cios com INNER JOIN
 
----(a)descrições dos produtos ao lados dos fabricantes
-SELECT descProduto 'Descrição do produto', nomeFabricante 'Fabricante' FROM tbProduto
+---(a)descriÃ§Ãµes dos produtos ao lados dos fabricantes
+SELECT descProduto 'DescriÃ§Ã£o do produto', nomeFabricante 'Fabricante' FROM tbProduto
 	INNER JOIN tbFabricante
 		ON tbProduto.idFabricante = tbFabricante.idFabricante
----(b)descrição dos produtos ao lado dos fornecedores
-SELECT descProduto 'Descrição do produto', nomeFornecedor 'Fornecedor' FROM tbProduto
+---(b)descriÃ§Ã£o dos produtos ao lado dos fornecedores
+SELECT descProduto 'DescriÃ§Ã£o do produto', nomeFornecedor 'Fornecedor' FROM tbProduto
 	INNER JOIN tbFornecedor
 		ON tbProduto.idFornecedor = tbFornecedor.idFornecedor
 ---(c)Listar soma das quantidades dos produtos agrupados pelo nome do fabricante
@@ -19,29 +20,29 @@ SELECT valorTotalVenda 'Total', nomeCliente 'Cliente' FROM tbVenda
 		ON tbVenda.idCliente = tbCliente.idCliente
 			--GROUP BY nomeCliente
 				--	ORDER BY nomeCliente
----(e)média dos preços dos produtos agrupados pelo nome do fabricante
-SELECT AVG(valorProduto) 'Média dos produtos', nomeFabricante 'Nome do Fabricante' FROM tbProduto
+---(e)mÃ©dia dos preÃ§os dos produtos agrupados pelo nome do fabricante
+SELECT AVG(valorProduto) 'MÃ©dia dos produtos', nomeFabricante 'Nome do Fabricante' FROM tbProduto
 	INNER JOIN tbFabricante
 		ON tbProduto.idFabricante = tbFabricante.idFabricante
 			GROUP BY nomeFabricante
----(f)listar todas as somas das vendas agrupadas pelo nome do cliente em ordem alfabética
+---(f)listar todas as somas das vendas agrupadas pelo nome do cliente em ordem alfabÃ©tica
 SELECT SUM(valorTotalVenda) 'Total', nomeCliente 'Cliente' FROM tbVenda
 	INNER JOIN tbCliente
 		ON tbVenda.idCliente = tbCliente.idCliente
 			GROUP BY nomeCliente
 					ORDER BY nomeCliente
----(g)listar a soma dos preços dos produtos agrupados pelo nome do fabricante
+---(g)listar a soma dos preÃ§os dos produtos agrupados pelo nome do fabricante
 SELECT SUM(valorProduto) 'Soma do valor do produto', nomeFabricante FROM tbProduto
 	INNER JOIN tbFabricante
 		ON tbProduto.idFabricante = tbFabricante.idFabricante
 			GROUP BY nomeFabricante
----(h)listar a  média dos preços dos produtos agrupados pelo nome do fornecedor
-SELECT AVG(valorProduto) 'Média dos valores dos produtos', nomeFornecedor FROM tbProduto
+---(h)listar a  mÃ©dia dos preÃ§os dos produtos agrupados pelo nome do fornecedor
+SELECT AVG(valorProduto) 'MÃ©dia dos valores dos produtos', nomeFornecedor FROM tbProduto
 	INNER JOIN tbFornecedor
 		 ON tbProduto.idFornecedor = tbFornecedor.idFornecedor
 			GROUP BY nomeFornecedor
 ---(i)listar a soma das vendas agrupadas pelo nome do produto
-SELECT SUM(valorTotalVenda) 'Valor Total', descProduto 'Descrição do Produto' FROM tbVenda
+SELECT SUM(valorTotalVenda) 'Valor Total', descProduto 'DescriÃ§Ã£o do Produto' FROM tbVenda
 	INNER JOIN tbItensVenda ON tbVenda.idVenda = tbItensVenda.idVenda
 		INNER JOIN tbProduto ON tbItensVenda.idProduto = tbProduto.idProduto
 			GROUP BY descProduto
